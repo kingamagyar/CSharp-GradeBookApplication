829f1eda2d1bb9fb7054c7c81bd86231130ca51e
Kinga Magyar <kinga.magyar89@gmail.com>
2018-03-04 20:59:05 +0100

    Completed BaseGradeBook's constructor

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -19,7 +19,7 @@ namespace GradeBook.GradeBooks
         public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
-            isWeighted = IsWeighted;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
